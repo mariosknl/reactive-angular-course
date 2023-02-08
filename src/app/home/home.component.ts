4af3aee1bebfcbb34eddb57abfa69aac580e0303
mariosknl <mariosknl@outlook.com>
2023-02-08 11:44:54 +0200

    refactored to presentational view component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -27,10 +27,7 @@ export class HomeComponent implements OnInit {
 
   advancedCourses$: Observable<Course[]>;
 
-  constructor(
-    private coursesService: CoursesService,
-    private dialog: MatDialog
-  ) {}
+  constructor(private coursesService: CoursesService) {}
 
   ngOnInit() {
     const courses$ = this.coursesService
@@ -48,17 +45,5 @@ export class HomeComponent implements OnInit {
         courses.filter((course) => course.category == "ADVANCED")
       )
     );
-  }
-
-  editCourse(course: Course) {
-    const dialogConfig = new MatDialogConfig();
-
-    dialogConfig.disableClose = true;
-    dialogConfig.autoFocus = true;
-    dialogConfig.width = "400px";
-
-    dialogConfig.data = course;
-
-    const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
   }
 }
