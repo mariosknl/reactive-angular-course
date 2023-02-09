c8f8df97661d775758191c3cdeb1c6f213375c1a
mariosknl <mariosknl@outlook.com>
2023-02-09 12:49:54 +0200

    angular master details implementation

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -4,6 +4,7 @@ import { Observable } from "rxjs";
 import { map, shareReplay } from "rxjs/operators";
 
 import { Course } from "../model/course";
+import { Lesson } from "../model/lesson";
 
 @Injectable({
   providedIn: "root",
@@ -22,5 +23,19 @@ export class CoursesService {
     return this.http
       .put(`/api/courses/${courseId}`, changes)
       .pipe(shareReplay());
+  }
+
+  searchLessons(search: string): Observable<Lesson[]> {
+    return this.http
+      .get<Lesson[]>("/api/lessons", {
+        params: {
+          filter: search,
+          pageSize: "100",
+        },
+      })
+      .pipe(
+        map((res) => res["payload"]),
+        shareReplay()
+      );
   }
 }
